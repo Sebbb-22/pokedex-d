083e8a3f35fcbb824ec53b83928a2d9cb23500fb
Sebastian Trejo <strejoh.10@icloud.com>
2026-03-04 19:58:54 -0600

    Show pokemons on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
-
+  const [results, setResults] = useState<any[]>([]);
   useEffect(() => {
-    console.log("Entre en pantalla")
+    console.log("Entre en pantalla");
     getPokemons();
-  }, [])
+  }, []);
 
   const getPokemons = async () => {
-    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(URL);
-    const data = await response.json()
-    console.log(data);
+    const data = await response.json();
+    setResults(data.results);
   };
 
   return (
     <View>
-      <Text>Sebastian</Text>
+      <Text>{results[3]?.name}</Text>
+      {results.map((item) => {
+        return <Text key={item.name}>{item.name}</Text>;
+      })}
     </View>
   );
 }
